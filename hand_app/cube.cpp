41a8bd669b5a0162037a3fb8585094568374f260
alban.lemiere@telecom-st-etienne.fr <alban.lemiere@telecom-st-etienne.fr>
2023-05-31 13:13:05 +0200

    logo avec la vraie épaisseur de mur

--- hand_app/cube.cpp
+++ hand_app/cube.cpp
@@ -14,10 +14,10 @@ void Cube::drawCubeTexture(float bottom_left_corner_x, float bottom_left_corner_
     // Face arrière
     glBegin(GL_QUADS);
     glColor3f(blue[0], blue[1], blue[2]);
-    glVertex3f(bottom_left_corner_x, bottom_left_corner_y, bottom_left_corner_z-2);
-    glVertex3f(bottom_left_corner_x + 1, bottom_left_corner_y, bottom_left_corner_z-2);
-    glVertex3f(bottom_left_corner_x + 1, bottom_left_corner_y + 1, bottom_left_corner_z-2);
-    glVertex3f(bottom_left_corner_x, bottom_left_corner_y + 1, bottom_left_corner_z-2);
+    glVertex3f(bottom_left_corner_x, bottom_left_corner_y, bottom_left_corner_z-depth);
+    glVertex3f(bottom_left_corner_x + 1, bottom_left_corner_y, bottom_left_corner_z-depth);
+    glVertex3f(bottom_left_corner_x + 1, bottom_left_corner_y + 1, bottom_left_corner_z-depth);
+    glVertex3f(bottom_left_corner_x, bottom_left_corner_y + 1, bottom_left_corner_z-depth);
     glEnd();
 
     // Face droite
@@ -25,8 +25,8 @@ void Cube::drawCubeTexture(float bottom_left_corner_x, float bottom_left_corner_
     glColor3f(0.1f, 0.2f, 0.3f);
     glVertex3f(bottom_left_corner_x + 1, bottom_left_corner_y, bottom_left_corner_z);
     glVertex3f(bottom_left_corner_x + 1, bottom_left_corner_y + 1, bottom_left_corner_z);
-    glVertex3f(bottom_left_corner_x + 1, bottom_left_corner_y + 1, bottom_left_corner_z-2);
-    glVertex3f(bottom_left_corner_x + 1, bottom_left_corner_y, bottom_left_corner_z-2);
+    glVertex3f(bottom_left_corner_x + 1, bottom_left_corner_y + 1, bottom_left_corner_z-depth);
+    glVertex3f(bottom_left_corner_x + 1, bottom_left_corner_y, bottom_left_corner_z-depth);
     glEnd();
 
     // Face bas
@@ -34,8 +34,8 @@ void Cube::drawCubeTexture(float bottom_left_corner_x, float bottom_left_corner_
     glColor3f(0.5f, 0.2f, 0.5f);
     glVertex3f(bottom_left_corner_x, bottom_left_corner_y, bottom_left_corner_z);
     glVertex3f(bottom_left_corner_x + 1, bottom_left_corner_y , bottom_left_corner_z);
-    glVertex3f(bottom_left_corner_x + 1, bottom_left_corner_y, bottom_left_corner_z-2);
-    glVertex3f(bottom_left_corner_x, bottom_left_corner_y, bottom_left_corner_z-2);
+    glVertex3f(bottom_left_corner_x + 1, bottom_left_corner_y, bottom_left_corner_z-depth);
+    glVertex3f(bottom_left_corner_x, bottom_left_corner_y, bottom_left_corner_z-depth);
     glEnd();
 
     // Face gauche
@@ -43,8 +43,8 @@ void Cube::drawCubeTexture(float bottom_left_corner_x, float bottom_left_corner_
     glColor3f(dark_grey[0], dark_grey[1], dark_grey[2]);
     glVertex3f(bottom_left_corner_x, bottom_left_corner_y, bottom_left_corner_z);
     glVertex3f(bottom_left_corner_x, bottom_left_corner_y + 1, bottom_left_corner_z);
-    glVertex3f(bottom_left_corner_x, bottom_left_corner_y + 1, bottom_left_corner_z-2);
-    glVertex3f(bottom_left_corner_x, bottom_left_corner_y, bottom_left_corner_z-2);
+    glVertex3f(bottom_left_corner_x, bottom_left_corner_y + 1, bottom_left_corner_z-depth);
+    glVertex3f(bottom_left_corner_x, bottom_left_corner_y, bottom_left_corner_z-depth);
     glEnd();
 
     // Face haut
@@ -52,8 +52,8 @@ void Cube::drawCubeTexture(float bottom_left_corner_x, float bottom_left_corner_
     glColor3f(grey[0], grey[1], grey[2]);
     glVertex3f(bottom_left_corner_x, bottom_left_corner_y + 1, bottom_left_corner_z);
     glVertex3f(bottom_left_corner_x + 1, bottom_left_corner_y + 1, bottom_left_corner_z);
-    glVertex3f(bottom_left_corner_x + 1, bottom_left_corner_y + 1, bottom_left_corner_z-2);
-    glVertex3f(bottom_left_corner_x, bottom_left_corner_y + 1, bottom_left_corner_z-2);
+    glVertex3f(bottom_left_corner_x + 1, bottom_left_corner_y + 1, bottom_left_corner_z-depth);
+    glVertex3f(bottom_left_corner_x, bottom_left_corner_y + 1, bottom_left_corner_z-depth);
     glEnd();
 
 //    // Face avant
